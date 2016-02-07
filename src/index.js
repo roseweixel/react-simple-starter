c55a57dffa5c8b785b91871ac4cf0797e94f9715
Rose Weixel <rose.weixel@gmail.com>
2016-02-07 18:54:35 -0500

    debounce search function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import Secrets from './secrets';
 import YouTubeSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
@@ -30,9 +31,11 @@ class App extends Component {
   }
 
   render () {
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={ term => this.videoSearch(term) } />
+        <SearchBar onSearchTermChange={ videoSearch } />
         <VideoDetail video={ this.state.selectedVideo } />
         <VideoList
           onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
