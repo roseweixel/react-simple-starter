c745125abfba1030a4df32cae8731247ced06278
Rose Weixel <rose.weixel@gmail.com>
2016-02-07 17:26:29 -0500

    add youtube api and search bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Secrets from './secrets';
+import SearchBar from './components/search-bar';
+
+const YOUTUBE_KEY = Secrets.youtube_key;
 
 // Create a new component that produces some HTML
 // this is a class, not an instance
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it in the DOM
